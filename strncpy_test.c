6bf0769ce87df3a283f0e5f54880b857b808f57c
asundark <ajaysundar.k@gmail.com>
2016-07-30 22:00:51 +0530

    strncpy vs strcpy

--- strncpy_test.c
+++ strncpy_test.c
@@ -3,13 +3,18 @@
 
 int main(int argc, char const *argv[])
 {
-	char str1[]= "Eth 1/1";
-	char str2[]= "Ve 40";
-    char str3[40] = {0};
+	char str1[]= "abc123";
+    char str2[4] = {0};
+    char str3[5] = "aa123";
 
-    strncpy(str3, str1, strlen(str1));
-    printf("%s\nLen: %d\n", str3, strlen(str3));
-    strcpy(str3, str2);
-    printf("%s\nLen: %d\n", str3,strlen(str3));
+    strncpy(str2, str1, strlen(str1));
+    printf("%s\nLen: %d\n\n", str2, strlen(str2)); /* This is why strncpy is dangerous, it will not append NULL here at the end, and could result in unexpected results*/
+
+    strncpy(str2, str3, strlen(str3));
+    str2[4] = '\0';
+    printf("%s\nLen: %d\n\n", str2, strlen(str2));
+
+    strcpy(str2, str1);
+    printf("%s\nLen: %d\n\n", str2,strlen(str2)); /* but, still, at some places, strcpy is better than strncpy */
 	return 0;
 }
